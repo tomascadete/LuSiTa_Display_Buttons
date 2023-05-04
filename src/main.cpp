32b29be73196c85bbb36156ee51a988155d98d45
Cadete <t.cadete24@gmail.com>
2023-05-04 15:43:43 +0100

    test

--- src/main.cpp
+++ src/main.cpp
@@ -63,6 +63,7 @@ void setup()
   pinMode(PIN_S3, INPUT_PULLUP);
   pinMode(PIN_S4, INPUT_PULLUP);
 
+
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
   { 
